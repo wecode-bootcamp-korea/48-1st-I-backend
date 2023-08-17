028e3f459c46cf2ca7c86075d69a641a6667f338
dodoyoo <dong15903@daum.net>
2023-08-17 17:42:18 +0900

    auth 3

--- src/routes/threadRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const threadController = require("../controllers/threadController");
-const {loginRequired} = require("../utils/auth");
-const threadRouter = express.Router();
-
-threadRouter.get("", threadController)
-module.exports = { threadRouter };
